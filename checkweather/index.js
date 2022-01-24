b6681e5591acc6e78c73820ae27d7062bbb7b746
Mayank Virmani <virmanimayank15@gmail.com>
2022-01-24 14:48:20 +0530

    Completed TODO App

--- checkweather/index.js
+++ checkweather/index.js
@@ -1,43 +1,54 @@
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // 11f76e6f8a32776a235e1e6bf8ba0393
-console.log("Weather forecast")
-var cityName = 'Jammu';
-const url="https://api.openweathermap.org/data/2.5/weather?q={cityName}+&appid=11f76e6f8a32776a235e1e6bf8ba0393";
+console.log("Weather forecast");
+var cityName = "Jammu";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}+&appid=11f76e6f8a32776a235e1e6bf8ba0393`;
 
-async function getdata(url){
-    const response = await fetch(url);
+async function getdata(url) {
+  const response = await fetch(url);
 
-    var data = await response.json();
-    // console.log(data);
-    console.log(data.sys.country);
-    console.log(data.name);
-    console.log(data.main);
-    console.log(data.main.temp);
-    console.log(data.main.pressure);
-    console.log(data.main.humidity);
+  var data = await response.json();
+  // console.log(data);
+  console.log(data.sys.country);
+  console.log(data.name);
+  console.log(data.main);
+  console.log(data.main.temp);
+  console.log(data.main.pressure);
+  console.log(data.main.humidity);
 
-    document.getElementById('city').innerHTML = data.sys.country + ", " + data.name;
-    document.getElementById('degree').innerHTML = data.main.temp + "<br>" + data.weather[0].main;
-    document.getElementById('pressure').innerHTML = "Pressure : " + data.main.pressure;
-    document.getElementById('humidity').innerHTML = "Humidity : " + data.main.humidity;
-    document.getElementById('visibility').innerHTML = "Visibility : " + data.visibility;
-
+  document.getElementById("city").innerHTML =
+    data.sys.country + ", " + data.name;
+  document.getElementById("degree").innerHTML =
+    data.main.temp + "<br>" + data.weather[0].main;
+  document.getElementById("pressure").innerHTML =
+    "Pressure : " + data.main.pressure;
+  document.getElementById("humidity").innerHTML =
+    "Humidity : " + data.main.humidity;
+  document.getElementById("visibility").innerHTML =
+    "Visibility : " + data.visibility;
 }
 
-
-var date=new Date();
+var date = new Date();
 // var day= date.toLocaleString("default", { day: "short" })
-var today=date.toUTCString().slice(5, 16);
+var today = date.toUTCString().slice(5, 16);
 // console.log(day.toString);
 console.log(today);
 // document.getElementById('date').innerHTML = day;
-document.getElementById('date').innerHTML = today;
+document.getElementById("date").innerHTML = today;
 
+function submitBtn() {
+  const cityBox = document
+    .getElementById("addlocation")
+    .getElementsByTagName("input");
+  const cityName = cityBox[0].value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}+&appid=11f76e6f8a32776a235e1e6bf8ba0393`;
+  getdata(url);
+}
 
-function changeLocation(){
-    document.getElementById('addlocation').innerHTML =  '<input type="text" placeholder="Enter city name">';
-    // cityName = text;
-    cityName = document.getElementById('addlocation').innerHTML;
-   
+function changeLocation() {
+  document.getElementById("addlocation").innerHTML =
+    '<input type="text" placeholder="Enter city name"> <button onclick="submitBtn()">Submit </button>';
+  // cityName = text;
+  //   cityName = document.getElementById("addlocation").innerHTML;
 }
 getdata(url);
